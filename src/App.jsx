21b7841ee4706d707e688590df21d7c22711acbd
SG_ <salahgamer936@gmail.com>
2025-11-26 00:25:25 +0100

    fixing the pathing v2

--- src/App.jsx
+++ src/App.jsx
@@ -31,21 +31,12 @@ const App = () => (
               </header>
               <main className="flex-1 p-6 bg-background">
                 <Routes>
-                  <Route path="/stockwise-dashboard/" element={<Dashboard />} />
-                  <Route
-                    path="/stockwise-dashboard/inventory"
-                    element={<Inventory />}
-                  />
-                  <Route
-                    path="/stockwise-dashboard/categories"
-                    element={<Categories />}
-                  />
-                  <Route
-                    path="/stockwise-dashboard/settings"
-                    element={<Settings />}
-                  />
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/inventory" element={<Inventory />} />
+                  <Route path="/categories" element={<Categories />} />
+                  <Route path="/settings" element={<Settings />} />
                   {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                  <Route path="/stockwise-dashboard/*" element={<NotFound />} />
+                  <Route path="/*" element={<NotFound />} />
                 </Routes>
               </main>
             </div>
